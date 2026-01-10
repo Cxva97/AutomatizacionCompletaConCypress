687e08bea76896f35fe355be08b539fcd3209114
Cxva97 <cxva97@hotmail.com>
2026-01-09 23:11:11 -0500

    registro

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportWidth: 1111,
+  viewportHeight: 691,
+  
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+    },
+    baseUrl: "https://www.laboratoriodetesting.com/",
+    retries: {
+      openMode: 2, // intentos en modo abierto
+      runMode: 1 // intentos en modo ejecucion, terminal
     },
   },
 });
